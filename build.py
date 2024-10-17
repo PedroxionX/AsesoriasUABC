0b03838c802c99f6a0f084a4ff9a767bf17943a9
PedroxionX <pedrocbcmp@gmail.com>
2024-10-17 15:25:35 -0700

    first commit

---
+++ build.py
@@ -0,0 +1,32 @@
+import PyInstaller.__main__
+import os
+import sys
+
+# Obtener el directorio actual
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Definir los archivos y carpetas a incluir
+main_file = os.path.join(current_dir, 'main.py')
+utils_dir = os.path.join(current_dir, 'utils')
+ico_dir = os.path.join(current_dir, 'ico')
+
+# Definir las opciones de PyInstaller
+options = [
+    main_file,
+    '--name=AsesoríasUABC',
+    '--onefile',
+    '--windowed',
+    f'--add-data={utils_dir}:utils',
+    f'--add-data={ico_dir}:ico',
+    '--icon=' + os.path.join(ico_dir, 'ico.png'),
+    '--hidden-import=babel.numbers',
+    '--hidden-import=tkcalendar',
+    '--hidden-import=CTkListbox',
+    '--debug=all',
+    '--log-level=DEBUG',
+]
+
+# Ejecutar PyInstaller
+PyInstaller.__main__.run(options)
+
+print("La construcción de la aplicación ha finalizado.")
